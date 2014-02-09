d8462a22621735b812d2bd0454fec1c4def02080
Edithson Abelard <eabelard@passion47.com>
2014-02-09 08:46:53 -0500

    Lesson 1: Intro to Slider Script

---
+++ Assets/Lesson1/Editor/SliderEdit.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+
+@CustomEditor(Slider)
+@CanEditMultipleObjects
+
+class SliderEdit extends Editor{
+	var _damage: SerializedProperty;
+	var _atkRate: SerializedProperty;
+	var _coins: SerializedProperty;
+
+
+	function OnEnable(){
+		_damage = serializedObject.FindProperty("damage");	
+		_atkRate = serializedObject.FindProperty("atkRate");	
+		_coins = serializedObject.FindProperty("coins");	
+	}
+	
+	function OnInspectorGUI(){
+		Debug.Log("Hit Gui");
+		//get current values 
+		serializedObject.Update();
+		//show the values
+		
+		EditorGUILayout.IntSlider(_damage, 0, 70, new GUIContent("Damage"));
+		EditorGUILayout.IntSlider(_atkRate, 0, 70, new GUIContent("Attack Rate"));
+		EditorGUILayout.IntSlider(_coins, 0, 70, new GUIContent("Coins"));
+		
+		
+		//update the values 
+		serializedObject.ApplyModifiedProperties();
+		
+		
+	}
+
+} 
---
+++ Assets/Lesson1/Slider.js
@@ -0,0 +1,5 @@
+﻿#pragma strict
+
+var damage: int  = 10;
+var atkRate: int = 40;
+var coins: int = 0;
