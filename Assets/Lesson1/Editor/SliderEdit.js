e9c23114f8bf5662266327ca1a602d40dd702163
Edithson Abelard <eabelard@passion47.com>
2014-02-17 11:33:20 -0500

    Lesson 1 part 2: Creating Messages and Toggles

--- Assets/Lesson1/Editor/SliderEdit.js
+++ Assets/Lesson1/Editor/SliderEdit.js
@@ -7,7 +7,7 @@ class SliderEdit extends Editor{
 	var _damage: SerializedProperty;
 	var _atkRate: SerializedProperty;
 	var _coins: SerializedProperty;
-
+	var _isHiddenMsg: boolean = false;
 
 	function OnEnable(){
 		_damage = serializedObject.FindProperty("damage");	
@@ -21,9 +21,19 @@ class SliderEdit extends Editor{
 		serializedObject.Update();
 		//show the values
 		
+	
 		EditorGUILayout.IntSlider(_damage, 0, 70, new GUIContent("Damage"));
+		EditorGUILayout.HelpBox("T This is the damage the user takes This is the damage the user takesThis is the damage the user takesThis is the damage the user takesThis is the damage the user takesThis is the damage the user takesThis is the damage the user takesThis is the damage the user takeshis is the damage the user takes", MessageType.None, false);
+		
 		EditorGUILayout.IntSlider(_atkRate, 0, 70, new GUIContent("Attack Rate"));
+		EditorGUILayout.HelpBox("Attack Rate will decrease over time", MessageType.Info, false);
 		EditorGUILayout.IntSlider(_coins, 0, 70, new GUIContent("Coins"));
+		EditorGUILayout.HelpBox("This value needs to be max 3 digits: Or else gui will break", MessageType.Warning, false);
+		
+		_isHiddenMsg = EditorGUILayout.Toggle("Show me hidden Message", _isHiddenMsg);
+		
+		EditorGUILayout.HelpBox("T This is the damage the user takes This is the damage the user takesThis is the damage the user takesThis is the damage the user takesThis is the damage the user takesThis is the damage the user takesThis is the damage the user takesThis is the damage the user takeshis is the damage the user takes", MessageType.Error, true);
+		
 		
 		
 		//update the values 
